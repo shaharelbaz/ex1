9c99bdf1c6af96ddc7716a44385912bffc168041
שחר <@shachar-PC>
2016-03-13 18:15:37 +0200

    3.c לא עובד שירלי ושחר המלכות

---
+++ Ex1Mivne/src/Graph_algo.java
@@ -0,0 +1,15 @@
+
+public class Graph_algo {
+	Graph G;
+
+	public Graph_algo(Graph G) {
+		this.G = G;
+	}
+	public double MinDistNodes(String name_file,int start,int end){
+		Graph g = new Graph(name_file,start);
+		return g.MinDistanceTwoNode(end);
+	}
+//	public double MinDistNodesWithBL(String name_file,int start,int end){
+//		
+//	}
+}
