ef8cb13e23b3d0871beb2a5fd6ab803c0dd42e4d
שחר <@shachar-PC>
2016-03-13 18:14:40 +0200

    3.c לא עובד. שירלי ושחר המלכות

--- Ex1Mivne/src/Graph.java
+++ Ex1Mivne/src/Graph.java
@@ -9,20 +9,25 @@ import java.util.*;
 
 public class Graph {
 
-	 final int infinity = Integer.MAX_VALUE;
-	 int t, nodes, edges,startNode;
-	 double[] distance;
-	 List<Edge>[] list;
-	 Scanner in = new Scanner(System.in);
-      
+	final int infinity = Integer.MAX_VALUE;
+	int t, nodes, edges,startNode;
+	double[] distance;
+	List<Edge>[] list;
+	List<Edge>[] back;
+	double[] DBlackList;
+	Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		Graph G = new Graph("tinyEWD.txt",0);
-	//	Graph G = new Graph("erezTest",1);
 		
-//		System.out.println("need to be true  " + (G.MinDistanceTwoNode(5)==20) );
-//		System.out.println("need to be true  " + (G.MinDistanceTwoNode(6)==11) );
-		System.out.println(G.getPath(5));
+	//	Graph G = new Graph("erezTest",4);
+		System.out.println(G.getPath(7));
+		for (int i=0;i<G.nodes;i++){
+			G.BlackGraph("BlackList.txt",i);
+			System.out.println(G.DBlackList[i]);
+		}
+		//Graph_algo g = new Graph_algo();
+	//	System.out.println(g.MinDistNodes("erezTest", 0, 5));
 	}
 
 	public Graph(String name_file,int start){
@@ -36,7 +41,7 @@ public class Graph {
 	}
 	private void createGraph(String Graph_name_file) {
 		String s = "";
-		
+
 		FileReader in;
 		try {
 			in = new FileReader(Graph_name_file);
@@ -44,6 +49,7 @@ public class Graph {
 			s = bf.readLine();
 			nodes = Integer.valueOf(s);
 			list = new ArrayList[nodes];
+			back = new ArrayList[nodes];
 			s = bf.readLine();
 			edges = Integer.valueOf(s);
 
@@ -77,26 +83,62 @@ public class Graph {
 				from = Integer.valueOf(x);
 				to = Integer.valueOf(y) ;
 				weight = temp;
-				
+
 				if (list[from] == null) {
-					list[from] = new ArrayList<>();
+					list[from] = new ArrayList<Edge>();
 				}
 
 				list[from].add(new Edge(to, weight));
-
-				if (list[to] == null) {
-					list[to] = new ArrayList<>();
+				
+			
+				if (back[to] == null) {
+					back[to] = new ArrayList<Edge>();
 				}
 
-				list[to].add(new Edge(from, weight));
+				back[to].add(new Edge(from, weight));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-public double MinDistanceTwoNode(int end){
-	return distance[end];
-}
+	//	public static void printPath (WeightedGraph G, int [] pred, int s, int e) {
+	//		       final java.util.ArrayList path = new java.util.ArrayList();
+	//		       int x = e;
+	//		       while (x!=s) {
+	//		         path.add (0, G.getLabel(x));
+	//		          x = pred[x];
+	//		       }
+	//		        path.add (0, G.getLabel(s));
+	//		       System.out.println (path);
+	//		    }
+
+	public double MinDistanceTwoNode(int end){
+		return distance[end];
+	}
+	public String getPath(int end){
+		int start=end;
+		String s="";
+		s=s+end;
+		Iterator<Edge> iterator ;
+		
+		while (start!=startNode){
+			iterator = back[start].iterator();
+			boolean b=true;
+			while (iterator.hasNext()&&b) {
+				Edge curr = iterator.next();
+				
+				if (Math.abs((this.distance[start]-curr.weight)-(this.distance[curr.to]))<0.00001)
+				{
+					start=curr.to;
+					s=curr.to+"->"+s;
+					b=false;
+				}
+			}	
+		}
+		return s;
+	}
+
+
 	private void findShortestPaths( int start) {
 		this.distance = new double[nodes];
 
@@ -108,59 +150,141 @@ public double MinDistanceTwoNode(int end){
 
 		PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
 		priorityQueue.add(new Node(start, 0, -1));
-
+	
 		while (priorityQueue.size() > 0) {
+			
 			Node min = priorityQueue.poll();
-			Iterator<Edge> iterator = list[min.node].iterator();
-
-			while (iterator.hasNext()) {
-				Edge curr = iterator.next();
-
-				if (distance[min.node] + curr.weight < distance[curr.to]) {
-					distance[curr.to] = distance[min.node] + curr.weight;
-					priorityQueue.add(new Node(curr.to, distance[curr.to], min.node));
-
+			if ( list[min.node]!=null){
+				Iterator<Edge> iterator = list[min.node].iterator();
+	
+				while (iterator.hasNext()) {
+					Edge curr = iterator.next();
+	
+					if (distance[min.node] + curr.weight < distance[curr.to]) {
+						distance[curr.to] = distance[min.node] + curr.weight;
+						priorityQueue.add(new Node(curr.to, distance[curr.to], min.node));
+	
+					}
 				}
 			}
 		}
 	}
 	
-	public String getPath(int end)
+	private void BlackGraph(String filename,int end)
 	{
-		System.out.println(this.list[7].get(this.list[7].size()-1).to);
-		int start=end;
-		String s="";
-		s=s+end;
-		while (start!=0)
-		{
-			
-			boolean b=true;
-			Iterator<Edge> iterator = list[start].iterator();
-			while (iterator.hasNext()&&b) {
+		String s = "";
+		this.DBlackList = new double[nodes];
+
+		for (int j = 0; j < nodes; j++) {
+			DBlackList[j] = infinity;
+		}
+
+		distance[startNode] = 0;
+
+		PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+		priorityQueue.add(new Node(startNode, 0, -1));
+
+		int numnode;
+		FileReader in;
+		try {
+			in = new FileReader(filename);
+			BufferedReader bf = new BufferedReader(in);
+			s = bf.readLine();
+			numnode = Integer.valueOf(s);
+
+			for (int i = 0; i < numnode; i++) {
+				s = bf.readLine();
+				int from, to;
+				int num;
+				String x = "", y = "", w = "";
+				int a = 0, b = 0;
+				int p=0;
+				for (p = 0; p < s.length(); p++) {
+					if (s.charAt(p) == ' ' && x.equalsIgnoreCase("")) {
+						x = s.substring(0, p);
+						a = p + 1;
+					} else if (s.charAt(p) == ' ' && a != 0) {
+						y = s.substring(a, p);
+						b = p + 1;
+					} else if ((s.charAt(p)==' '||p==s.length()-1)&& a != 0 && b != 0) {
+						w = s.substring(b, p);
+						break;
+					}
+				}
+				from = Integer.valueOf(x);
+				to = Integer.valueOf(y) ;
+				num=Integer.valueOf(w);
+				x="";
+				a=p;
+				int number;
+				if (num>0&&from==startNode&&to==end){
+					for (int k=p;k<s.length();k++)
+					{
+						if (s.charAt(k)==' '||k==s.length()-1)
+						{
+							x=s.substring(a, k);
+							a=k+1;
+							number=Integer.valueOf(x);
+							list[number]=null;
+							back[number]=null;
+						}
+					}
+				}
 				
-				Edge curr = iterator.next();
-				if (this.distance[start]-curr.weight==this.distance[curr.to])
-				{
-					start=curr.to;
-					s=curr.to+"->"+s;
-					b=false;
+			
+				while (priorityQueue.size() > 0) {
+					
+					Node min = priorityQueue.poll();
+					if ( list[min.node]!=null){
+						Iterator<Edge> iterator = list[min.node].iterator();
+			
+						while (iterator.hasNext()) {
+							Edge curr = iterator.next();
+			
+							if (DBlackList[min.node] + curr.weight < DBlackList[curr.to]) {
+								DBlackList[curr.to] = DBlackList[min.node] + curr.weight;
+								priorityQueue.add(new Node(curr.to, DBlackList[curr.to], min.node));
+			
+							}
+						}
+					}
 				}
 			}
-			
-				
-			
-			
 		}
-		return s;
+		
+				
+				
+
+				
+				
+	 catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	static class Edge {
+	static class Edge implements Comparable<Edge> {
 		int to;
 		double weight;
 
 		public Edge(int to, double weight) {
 			this.to = to;
 			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge a) {
+	      return Double.compare(this.weight,a.weight );
+		}
+	}
+	
+	static class Black
+	{
+		int to;
+		int[]k;
+		public Black(int to, int num)
+		{
+			this.k=new int[num];
+			this.to=to;
 		}
 	}
 
